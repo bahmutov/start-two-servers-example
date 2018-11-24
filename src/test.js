f27260c682882a7449bf8f470b007a18abd16cbf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-11-24 16:46:39 +0000

    testing both api and server endpoints

--- src/test.js
+++ src/test.js
@@ -1,9 +1,20 @@
 const got = require('got')
 
-got('http://localhost:5000')
-.then(serverResponse => {
-  console.log(serverResponse.body);
-})
+// in order for test to pass both API and SERVER endpoints have to respond
+const apiEndpoint = 'http://localhost:7600'
+const serverEndpoint = 'http://localhost:5000'
+
+Promise.all([
+  got(apiEndpoint)
+  .then(apiResponse => {
+    console.log('API SAYS:', apiResponse.body);
+  }),
+
+  got(serverEndpoint)
+  .then(serverResponse => {
+    console.log('SERVER SAYS:', serverResponse.body);
+  })
+])
 .catch(error => {
   console.log(error);
   process.exit(1)
